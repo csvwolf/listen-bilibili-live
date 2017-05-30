704e3cb7bb9dedb707ae62a96f180220ae8df562
SkyAo <csvwolf@qq.com>
2017-05-31 01:35:42 +0800

    feat: add log to console

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const sendMail = require('./send-mail');
 const listenLive = require('./listen');
 const mailConfig = require('./config.json').mail
 const fs = require('fs')
+const path = require('path')
 
 const LIVE_ID = 691468
 
@@ -9,9 +10,13 @@ listenLive(LIVE_ID)
   .then(({ data }) => data.data)
   .then(data => {
     const status = require('./status.json')
-    if (status.rooms[LIVE_ID] === data.LIVE_STATUS) return {}
+    if (status.rooms[LIVE_ID] === data.LIVE_STATUS) {
+      console.log(new Date(), 'not changed')
+      return {}
+    }
     status.rooms[LIVE_ID] = data.LIVE_STATUS
-    fs.writeFile('./status.json', JSON.stringify(status), () => '')
+    console.log(new Date(), 'send mail...')
+    fs.writeFile(path.resolve('.') + '/status.json', JSON.stringify(status), () => '')
     return sendMail(mailConfig, {
       subject: `你所监听的主播 ${data.ANCHOR_NICK_NAME} 状态改变`,
       html: `\
